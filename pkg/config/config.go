d642bfd1a22a9ccd6dc847fe7cddf59fe2adad41
Hrithik S Raj <84892078+Hrithik-s-Raj@users.noreply.github.com>
2025-05-06 09:18:43 +0000

    Local Env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,29 +1,30 @@
 package config
 
 import (
-    "log"
-    "os"
-    "github.com/spf13/viper"
+	"log"
+
+	"github.com/spf13/viper"
 )
 
 func LoadEnv(env string) {
-    viper.SetConfigName(env)
-    viper.SetConfigType("env")
-    viper.AddConfigPath(".")
-    if err := viper.ReadInConfig(); err != nil {
-        log.Fatalf("Error reading config file: %v", err)
-    }
+	println(env)
+	viper.SetConfigName(env)
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
 
-    viper.AutomaticEnv()
+	viper.AutomaticEnv()
 }
 
 func GetPort() string {
-    if port := viper.GetString("PORT"); port != "" {
-        return port
-    }
-    return "8080"
+	if port := viper.GetString("PORT"); port != "" {
+		return port
+	}
+	return "8080"
 }
 
 func GetMongoURI() string {
-    return viper.GetString("MONGODB_URI")
+	return viper.GetString("MONGODB_URI")
 }
